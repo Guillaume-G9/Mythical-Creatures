66034d70bd09dd1d0e7d66361742a5f39bda9ee0
Patrick Goulding <patrickgoulding@gmail.com>
2019-04-08 13:27:17 -0600

    rework sphinx

--- mythical-creatures/exercises/sphinx.js
+++ mythical-creatures/exercises/sphinx.js
@@ -2,18 +2,26 @@ class Sphinx{
   constructor(){
     this.name = null;
     this.riddles = [];
-  } collectRiddle(riddle){
+    this.heroesEaten = 0;
+  } 
+  collectRiddle(riddle){
     this.riddles.push(riddle);
     if(this.riddles.length > 3){
     this.riddles.shift();
     } 
-  } attemptAnswer(attempt) {
-    riddles.array.forEach(element => {
-    
-    });
-    if (attempt === this.riddles[attempt].answer){
-      this.riddles.splice(attempt,1)
-    }
+  } 
+  attemptAnswer(attempted) {
+    this.riddles.forEach(i => {
+      if (attempted === this.riddles.answer){
+      this.riddles.splice(i,1)
+    };
+    })
+  }
+  response(){
+    return `That wasn't that hard, I bet you don't get the next one`;
+  }
+  rage(answer){
+    return `PSSSSSSS THIS HAS NEVER HAPPENED, HOW DID YOU KNOW THE ANSWER WAS ${answer}`;
   }
 }
 
